d2c43df84f1d253550df80abfa91b627ac91c0c4
arya <aryakhadakkar03@gmail.com>
2021-06-22 13:56:30 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,26 @@
+/*function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}*/
+var a = prompt('Enter the first variable: ');
+var b = prompt('Enter the second variable: ');
+function setup(){
+  var b2 = createButton("click here to swap");
+  b2.mousePressed(swap);
+}
+
+function draw()
+{
+  
+}
+
+function swap(){
+[a, b] = [b, a];
+
+console.log("This value of a after swapping:" ,+a);
+console.log("This value of b after swapping:" ,+b);
+
+}
